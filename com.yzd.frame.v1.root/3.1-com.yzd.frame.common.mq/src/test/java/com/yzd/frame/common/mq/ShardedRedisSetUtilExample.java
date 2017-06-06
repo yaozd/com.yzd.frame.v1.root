26988c3c640c5434382fecc9a29cc586a2b80d46
yzd <tedijoteamo@163.com>
2017-06-06 17:13:18 +0800

    订单类型消息队列-示例 1-任务消息添加 2-任务消息处理 3-任务消息排重

--- com.yzd.frame.v1.root/3.1-com.yzd.frame.common.mq/src/test/java/com/yzd/frame/common/mq/ShardedRedisSetUtilExample.java
+++ com.yzd.frame.v1.root/3.1-com.yzd.frame.common.mq/src/test/java/com/yzd/frame/common/mq/ShardedRedisSetUtilExample.java
@@ -46,20 +46,69 @@ public class ShardedRedisSetUtilExample {
         System.out.println(String.format("key:%s,count:%s,size:%s", key, count, setList.size()));
     }
 
-    @Test
-    public void orderMQ_add_Example() {
-        ShardedRedisMqUtil redisUtil = ShardedRedisMqUtil.getInstance();
-        for (int i = 0; i < 100; i++) {
-            String uuid=UUID.randomUUID().toString();
-            long countOfsadd = redisUtil.sadd(orderMQ_set01, uuid);
-            long countOflpush = redisUtil.lpush(orderMQ_list01, uuid);
-            System.out.println(String.format("countOfsadd:%s,countOflpush:%s", countOfsadd,countOflpush));
-        }
-    }
+    //region 订单类型消息队列-示例
     //------------------
     private String orderMQ_set01 = "orderMQ_set01";
     private String orderMQ_list01 = "orderMQ_list01";
     private String orderMQ_MutexKey01 = "orderMQ_MutexKey01:";
+
+    /**
+     * 1 orderMQ-set-排重消息队列
+     * 2 orderMQ-list-任务消息队列
+     * 3 orderMQ-MutesKey-互斥锁-记录正在运行的任务-有效时间为5分钟-任务完成后主动删除MutesKey
+     */
+
+    /**
+     * 消息队列-插入消息
+     */
+    @Test
+    public void orderMQ_add_Example() {
+        ShardedRedisMqUtil redisUtil = ShardedRedisMqUtil.getInstance();
+        for (int i = 0; i < 100; i++) {
+            String uuid= UUID.randomUUID().toString();
+            //todo 先插入到排重set消息队列中
+            long countOfsadd = redisUtil.sadd(orderMQ_set01, uuid);
+            //todo 判断当前消息是否存在-如果当前消息不存在，则插入到list消息队列中
+            if(countOfsadd==1){
+                long countOflpush = redisUtil.lpush(orderMQ_list01, uuid);
+                System.out.println(String.format("countOfsadd:%s,countOflpush:%s", countOfsadd,countOflpush));
+            }
+        }
+    }
+
+    /**
+     * 消息队列-头部读取
+     * 阻塞列表
+     * 移出并获取列表的【第一个元素】， 如果列表没有元素会阻塞列表直到等待超时或发现可弹出元素为止。
+     * todo 需要注意解决的两个异常：1-reids本身的异常 2-任务的操作异常 3-共同的网络中断产生的网络异常
+     */
+    @Test
+    public void orderMQ_blpop_Example() {
+        ShardedRedisMqUtil redisUtil = ShardedRedisMqUtil.getInstance();
+        for (int i=0;i<10000;i++){
+            ////todo Redis消息队列网络异常
+            //当超期时间到达时，keys列表仍然没有内容，则返回Null
+            String taskId= redisUtil.blpop(5, orderMQ_list01);
+            System.out.println(taskId);
+            if(taskId==null){
+                continue;
+            }
+            taskId="dc29e81a-f7d5-4bad-b2d2-3a0c6da729ca";
+            //todo 设置任务正在运行-有效时间为5分钟
+            String mutexKey=orderMQ_MutexKey01 + taskId;
+            String isOk= redisUtil.set(mutexKey, "1", "NX", "EX", 60 * 5);
+            if(!"OK".equals(isOk)){
+                continue;
+            }
+            //todo 具体任务操作
+            //todo 任务操作异常或数据库异常
+            //第一步先验证数据库-当前的任务是否已经处理
+            //-----------------------------------------
+            //todo 先删除set排除消息对列再删除mutexKey互斥锁
+            redisUtil.srem(orderMQ_set01,taskId);
+            //redisUtil.del(mutexKey);
+        }
+    }
 
     /**
      * 5分钟循环定时任务--示例
@@ -92,4 +141,5 @@ public class ShardedRedisSetUtilExample {
             System.out.println(valOfSrem);
         }
     }
+    //endregion
 }
