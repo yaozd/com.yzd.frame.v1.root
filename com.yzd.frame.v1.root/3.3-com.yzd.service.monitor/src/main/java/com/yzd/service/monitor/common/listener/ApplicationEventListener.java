cd1c9d9181429b35874c4eb3fb6a2c9aa20b7115
yzd <tedijoteamo@163.com>
2018-08-28 10:29:45 +0800

    监听Spring Boot的启动、停止、重启、关闭

--- com.yzd.frame.v1.root/3.3-com.yzd.service.monitor/src/main/java/com/yzd/service/monitor/common/listener/ApplicationEventListener.java
+++ com.yzd.frame.v1.root/3.3-com.yzd.service.monitor/src/main/java/com/yzd/service/monitor/common/listener/ApplicationEventListener.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.web.context.support.ServletRequestHandledEvent;
 
 /***
  *
@@ -16,9 +17,30 @@ import org.springframework.context.event.ContextClosedEvent;
 @Slf4j
 public class ApplicationEventListener implements ApplicationListener {
 
+    /***
+     * 监听Spring Boot的启动、停止、重启、关闭
+     * http://blog.sina.com.cn/s/blog_70ae1d7b0102wfq2.html
+     * // 在这里可以监听到Spring Boot的生命周期
+     * if (event instanceof ApplicationEnvironmentPreparedEvent) { // 初始化环境变量 }
+     * else if (event instanceof ApplicationPreparedEvent) { // 初始化完成 }
+     * else if (event instanceof ContextRefreshedEvent) { // 应用刷新 }
+     * else if (event instanceof ApplicationReadyEvent) {// 应用已启动完成 }
+     * else if (event instanceof ContextStartedEvent) { // 应用启动，需要在代码动态添加监听器才可捕获 }
+     * else if (event instanceof ContextStoppedEvent) { // 应用停止 }
+     * else if (event instanceof ContextClosedEvent) { // 应用关闭 }
+     * @param applicationEvent
+     */
     @Override
     public void onApplicationEvent(ApplicationEvent applicationEvent) {
         log.info("XXX"+applicationEvent.toString());
+        //ServletRequestHandledEvent 请求处理事件
+        //XXXServletRequestHandledEvent: url=[/]; client=[0:0:0:0:0:0:0:1]; method=[GET]; servlet=[dispatcherServlet]; session=[null]; user=[null]; time=[1ms]; status=[OK]
+        //===================================================
+        if(applicationEvent instanceof ServletRequestHandledEvent){
+            //一般怦是不会添加此事件监听
+            log.info("ServletRequestHandledEvent 请求处理事件");
+            return;
+        }
         //应用关闭-kill PID 不要使用kill -9 PID
         if (applicationEvent instanceof ContextClosedEvent){
             log.info("程序关闭发送所有监控数据");
@@ -26,7 +48,7 @@ public class ApplicationEventListener implements ApplicationListener {
             ServiceMonitorUtil.shutdownEvent();
             ServiceMonitorUtil.sendAllData();
             //
-
+            return;
         }
     }
 }
