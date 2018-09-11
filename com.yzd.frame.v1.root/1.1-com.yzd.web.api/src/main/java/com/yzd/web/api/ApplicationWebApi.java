c712f25d62fab96bb97bce0bb8b54a00815ceb8d
yzd <tedijoteamo@163.com>
2018-09-11 18:02:04 +0800

    update  API Controller

--- com.yzd.frame.v1.root/1.1-com.yzd.web.api/src/main/java/com/yzd/web/api/ApplicationWebApi.java
+++ com.yzd.frame.v1.root/1.1-com.yzd.web.api/src/main/java/com/yzd/web/api/ApplicationWebApi.java
@@ -1,8 +1,20 @@
 package com.yzd.web.api;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 /**
  * Created by Administrator on 2017/2/24.
+ * http://localhost:8890/
  */
-public class ApplicationWebApi {
-    public static void main(String[] args) {}
+@SpringBootApplication
+public class ApplicationWebApi  extends SpringBootServletInitializer {
+    /**
+     * Used when run as JAR
+     */
+    public static void main(String[] args) {
+
+        SpringApplication.run(ApplicationWebApi.class, args);
+    }
 }
--- com.yzd.frame.v1.root/1.1-com.yzd.web.api/src/main/java/com/yzd/web/api/controllerApi/_TestController.java
+++ com.yzd.frame.v1.root/1.1-com.yzd.web.api/src/main/java/com/yzd/web/api/controllerApi/_TestController.java
@@ -1,7 +1,19 @@
 package com.yzd.web.api.controllerApi;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by Administrator on 2017/2/24.
  */
+@RestController
+@RequestMapping("/api/test")
 public class _TestController {
+    @RequestMapping("doLogin")
+    public String doLogin() {
+        return "test/doLogin";
+    }
 }
+
